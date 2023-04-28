255f5f5a288d659e9c25bb27a1bf35e191a77980
TheLudway <70478991+TheLudway@users.noreply.github.com>
2023-04-28 17:14:22 -0500

    Add files via upload

---
+++ MÚSICA.py
@@ -0,0 +1,38 @@
+"""MUSICA PARA EL JUEGO"""
+import pygame
+
+pygame.init()
+def default_music():
+    """La música de ambiente por 12 horas"""
+    pygame.mixer.music.load('MÚSICA/Música_Default.mp3')
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+    pygame.mixer.music.stop()
+
+
+def comer_sound(): 
+    """Reproduce por 9 segundos el efecto de comer de minecraft"""
+    pygame.mixer.music.load('MÚSICA/Comer.mp3')
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+    pygame.mixer.music.stop()
+
+
+def mimir_sound():
+    """Reproduce por 7 segundos el sonido de dormir"""
+    pygame.mixer.music.load('MÚSICA/Mimir.mp3')
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+    pygame.mixer.music.stop()
+
+
+def morir_sound():
+    """Reproduce por 10 segundos plin plin plon"""
+    pygame.mixer.music.load('MÚSICA/Morir.mp3')
+    pygame.mixer.music.play()
+    pygame.time.wait(10000)
+    pygame.mixer.music.stop()
+
