357558ce6b6b2dd11b417d493a21d2831b168117
dangod881 <131696115+dangod881@users.noreply.github.com>
2023-05-14 20:09:53 -0500

    Delete PRUEBA.py

--- PRUEBA.py
+++
@@ -1,9 +0,0 @@
-"""
-INICIO DEL PROYECTO DEL SEMESTRE DE ALGORITMOS
-"""
-print("Esto es una línea de prueba")
-print("Segunda línea de prueba")
-print("Tercera línea de prueba")
-
-for i in range(2):
-    print(i)
