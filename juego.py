c1582656024ffc531941e0b13ee3d21f5c5ee098
dangod881 <131696115+dangod881@users.noreply.github.com>
2023-05-14 20:06:59 -0500

    Add files via upload

--- juego.py
+++ juego.py
@@ -1,4 +1,4 @@
-import pygame, time, musica
+import pygame, random, time, musica
 pygame.init()
 
 tama = (400, 460)
@@ -7,9 +7,10 @@ reloj = pygame.time.Clock()
 a= 0.9
 
 #importar imagenes
-personaje= pygame.image.load('personaje.jpg')
-personaje2= pygame.image.load('personaje2.jpg')
-personaje_sad= pygame.image.load('personaje_sad.jpg')
+personaje= pygame.image.load('cabocommon.jpg')
+personaje2= pygame.image.load('cabocommon2.jpg')
+personaje_sad= pygame.image.load('cabosad.jpg')
+personaje_happy= pygame.image.load('cabohappy.jpg')
 comer1=pygame.image.load('spriteC1.jpg')
 comer2=pygame.image.load('spriteC2.jpg')
 dormir1=pygame.image.load('dormir_1.jpg')
@@ -20,36 +21,25 @@ pelota2=pygame.image.load('pelota2.jpg')
 pelota3=pygame.image.load('pelota3.jpg')
 muerto=pygame.image.load('muerto.jpg')
 
-#para colores
+
+
+#ubicacion personaje
+x= 130
+y= 140
+
+#para botones
 red=(205,30,0)
 red_on= (250,40,1)
 blue=(0,0,205)
 blue_on=(135, 206, 250)
 gold=(255, 215, 0)
 gold_on=(218, 165, 32)
-black=(0,0,0)
-white= (255,255,255)
-        
-#textos
-fuente= pygame.font.SysFont("Small Fonts", 20)
-fuente2 = pygame.font.SysFont("White On",30)
-texto1 = fuente.render("Vida", True, black)
-texto2 = fuente.render("Energia", True, black)
-texto3 = fuente.render("Hambre", True, black)
-texto4 = fuente.render("Felicidad", True, black)
-texto5 = fuente.render("Dormir", True, black)
-texto6 = fuente.render("Comer", True, black)
-texto7 = fuente.render("Jugar", True, black)
-texto8 = fuente2.render("Game Over", True, red)
 
-#ubicacion personaje
-x= 130
-y= 140
 
 #boton
 m= 200
 n= 410
-az= 100 
+az= 100
 azo= 410
 ad= 300
 do= 410
@@ -159,16 +149,13 @@ class gochi:
             reloj.tick(a)
     
     def game_over(self):
-        if self.salud < 0:          
-            screen.blit(muerto,(x-50,y-20))
-            screen.blit(texto8, (200,50))
-
+        if self.salud < 0:
+            screen.blit(muerto,(x-20,y-20))
             pygame.display.update()
             reloj.tick(0.05)
+            
             return False
-        if self.salud == 0:
-            self.felicidad = 0
-            self.energia = 0
+
            
 acti = True
 tamama = gochi()
@@ -190,27 +177,17 @@ while acti:
             tamama.limite()
             
             print(tamama.salud, tamama.energia, tamama.lleno, tamama.felicidad)
-    screen.fill(white)
+    screen.fill((255,255,255))
 
-#Creacion de botones, barras de estado
-  
+       
+
     boton=pygame.draw.circle(screen, red,(m,n), 20)
     boton1=pygame.draw.circle(screen, blue,(az,azo), 20)
     boton2=pygame.draw.circle(screen, gold,(ad,do), 20)
-
-    screen.blit(texto1, (115, 10))
-    screen.blit(texto2, (115, 30))
-    screen.blit(texto3, (115, 50))
-    screen.blit(texto4, (115, 70))
-    screen.blit(texto5, (az-20,azo+20))
-    screen.blit(texto6, (m-20,n+20))
-    screen.blit(texto7, (ad-20,do+20))
-    
-
-    pygame.draw.rect(screen, black, [9, 9, 102, 12])
-    pygame.draw.rect(screen, black, [9, 29, 102, 12])
-    pygame.draw.rect(screen, black, [9, 49, 102, 12])
-    pygame.draw.rect(screen, black, [9, 69, 102, 12])
+    pygame.draw.rect(screen, (0, 0, 0), [9, 9, 102, 12])
+    pygame.draw.rect(screen, (0, 0, 0), [9, 29, 102, 12])
+    pygame.draw.rect(screen, (0, 0, 0), [9, 49, 102, 12])
+    pygame.draw.rect(screen, (0, 0, 0), [9, 69, 102, 12])
 
     pygame.draw.rect(screen, (0,255,0), [10, 10, tamama.salud, 10])
     pygame.draw.rect(screen, (0, 191, 255), [10, 30, tamama.energia, 10])
@@ -218,8 +195,7 @@ while acti:
     pygame.draw.rect(screen, (255, 215, 0), [10, 70, tamama.felicidad, 10])
 
     pygame.display.flip()    
-
-#personaje default
+   
     screen.blit(personaje,(x,y))
     pygame.display.update()
     reloj.tick(a)
@@ -227,7 +203,6 @@ while acti:
     pygame.display.update()
     reloj.tick(a)
 
-#reduccion estadisticas y game over
     golpe += 1
     if golpe == 2:
         golpe = 0
